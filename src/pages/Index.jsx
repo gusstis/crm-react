dda5ec32cecd24a7fb9f0bfebdb365d47e5af8ff
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-23 10:48:07 -0300

    Hardcoding clientes, getting loader data with useLoaderData

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,54 @@
-//import React from 'react'
+import {useLoaderData} from 'react-router-dom'
 
 // En React RouterDOM, para cargar el estado de clientes no usamos useEffect(). Loader siempre debe retornar algo
 export function loader() {
-  return 'Desde Loader';
+
+  const clientes = [
+    {
+        id: 1,
+        nombre: 'Juan',
+        telefono: 102013313,
+        email: "juan@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 2,
+        nombre: 'Karen',
+        telefono: 138198313,
+        email: "karen@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 3,
+        nombre: 'Josue',
+        telefono: 31983913,
+        email: "josue@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 4,
+        nombre: 'Miguel',
+        telefono: 319381983,
+        email: "miguel@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 5,
+        nombre: 'Pedro',
+        telefono: 1398198938,
+        email: "pedro@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+];
+
+  return clientes;
 }
 
 function Index() {
+
+  const clientes = useLoaderData();
+  console.log(clientes)
+
   return (
     <div>
       <>
