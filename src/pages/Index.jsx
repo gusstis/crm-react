90a97d2bf355d2134a5a5397f7b6e075b556a668
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-23 10:35:29 -0300

    Creating a loader

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,18 @@
 //import React from 'react'
 
+// En React RouterDOM, para cargar el estado de clientes no usamos useEffect(). Loader siempre debe retornar algo
+export function loader() {
+  return 'Desde Loader';
+}
+
 function Index() {
   return (
-    <div>Clientes</div>
+    <div>
+      <>
+        <h1 className="font-black text-blue-900" >Clientes</h1>
+        <p className="mt-3" >Administra tus Clientes</p>
+      </>
+    </div>
   )
 }
 
