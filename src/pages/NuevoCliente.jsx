5d02f7e326da64cd649788a6283caf72e5aca069
Gustavo Aranciba <gusaranciba@gmail.com>
2023-09-28 20:18:09 -0300

    Validation to Formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,16 +1,31 @@
-import {useNavigate, Form} from 'react-router-dom';
+import {useNavigate, Form, useActionData} from 'react-router-dom';
 import Formulario from '../components/Formulario';
+import Error from '../components/Error';
 
 //Con esto react-router-dom se encarga del envío de formularios
 export async function action ({request}) {
   const formData = await request.formData()
   const datos = Object.fromEntries(formData)
-  console.log(datos)
-  return datos
+  
+  //Validación:
+  const errores = []
+  if (Object.values(datos).includes('')) {
+    errores.push('Todos los campos son obligatorios')
+
+}
+
+//Retornar datos si hay errores
+if(Object.keys(errores).length) {
+  return errores
+}
+
+
+  //return null
 }
 
 function NuevoCliente() {
 
+  const errores = useActionData()
   const navigate = useNavigate()
 
   return (
@@ -26,6 +41,8 @@ function NuevoCliente() {
             </button>
         </div>
         <div className=' bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20 '>
+          
+          { errores?.length && errores.map ((error, i) => <Error key={i} >{error}</Error> ) }
           <Form
             method='post'
           >
