f415e434e045c640843adcbe7f684f7aeaf5ff12
Gustavo Aranciba <gusaranciba@gmail.com>
2023-10-02 17:34:57 -0300

    Adding new action() for EditarCliente

--- src/data/clientes.js
+++ src/data/clientes.js
@@ -30,3 +30,19 @@ export async function agregarCliente (datos) {
     }
     
 }
+
+export async function actualizarCliente (id, datos) {
+    try {
+        const respuesta = await fetch( `${import.meta.env.VITE_API_URL}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        } )
+        await respuesta.json()
+        //return true;
+    } catch (error) {
+        console.log(error)
+    }
+}
